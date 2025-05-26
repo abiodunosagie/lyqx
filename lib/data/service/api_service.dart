7d39a91bb7efe72ca9ba5480164a91f5a66f19d8
abiodunosagie <abiodunosagie8@gmail.com>
2025-05-26 10:05:21 +0100

    changes.

--- lib/data/service/api_service.dart
+++ lib/data/service/api_service.dart
@@ -5,7 +5,9 @@ import '../api_config.dart';
 
 @lazySingleton
 class ApiService {
-  final Dio _dio = Dio(BaseOptions(baseUrl: ApiConfig.baseUrl));
+  final Dio _dio = Dio(
+    BaseOptions(baseUrl: ApiConfig.baseUrl),
+  );
 
   Future<List<dynamic>> getProducts() async {
     final response = await _dio.get("/products");
